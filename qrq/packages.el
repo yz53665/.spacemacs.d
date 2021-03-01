8f726be9b70e94c2f2454c6785196b95d2b2ae42
“yz53665” <875423353@qq.com>
2021-03-02 01:17:33 +0800

    configuration of pyim

--- qrq/packages.el
+++ qrq/packages.el
@@ -33,6 +33,7 @@
   '(
     org-roam-server
     (org-roam-server :location elpa)
+    pyim
     )
   "The list of Lisp packages required by the qrq layer.
 
@@ -79,5 +80,58 @@ Each entry is either:
     (require 'org-roam-protocol))
     (add-hook 'after-init-hook 'org-roam-mode)
   )
+(defun qrq/pre-init-pyim ()
+  (use-package pyim
+  :ensure nil
+  :demand t
+  :config
+  ;; 激活 basedict 拼音词库，五笔用户请继续阅读 README
+  ;; (use-package pyim-basedict
+  ;;   :ensure nil
+  ;;   :config (pyim-basedict-enable))
+
+  (setq default-input-method "pyim")
+
+  ;; 我使用全拼
+  (setq pyim-default-scheme 'quanpin)
+
+  ;; 设置 pyim 探针设置，这是 pyim 高级功能设置，可以实现 *无痛* 中英文切换 :-)
+  ;; 我自己使用的中英文动态切换规则是：
+  ;; 1. 光标只有在注释里面时，才可以输入中文。
+  ;; 2. 光标前是汉字字符时，才能输入中文。
+  ;; 3. 使用 M-j 快捷键，强制将光标前的拼音字符串转换为中文。
+  (setq-default pyim-english-input-switch-functions
+                '(pyim-probe-dynamic-english
+                  pyim-probe-isearch-mode
+                  pyim-probe-program-mode
+                  pyim-probe-org-structure-template))
+
+  (setq-default pyim-punctuation-half-width-functions
+                '(pyim-probe-punctuation-line-beginning
+                  pyim-probe-punctuation-after-punctuation))
+
+  ;; 开启拼音搜索功能
+  (pyim-isearch-mode 1)
+
+  ;;设置词条转功能
+  (global-set-key (kbd "M-f") 'pyim-forward-word)
+  (global-set-key (kbd "M-b") 'pyim-backward-word)
+
+  ;; 使用 posframe 绘制 page, (需要用户手动安装 posframe 包）。
+  ;; (setq pyim-page-tooltip 'posframe)
+
+  ;; 如果 posframe 不可用，可以试着安装 popup 包，然后设置：
+  ;; ;; (setq pyim-page-tooltip 'popup)
+
+  ;; 选词框显示 5 个候选词
+  (setq pyim-page-length 5)
+  (define-key pyim-mode-map (kbd "C-j") 'pyim-page-next-page)
+  (define-key pyim-mode-map (kbd "C-k") 'pyim-page-previous-page)
+
+  :bind
+  (
+   ("C-," . pyim-convert-string-at-point) ;与 pyim-probe-dynamic-english 配合
+   )))
+
 
 ;;; packages.el ends here
